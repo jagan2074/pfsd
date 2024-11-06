f8f00001fd47dd8c8fd9148b8727a5454a743f7b
jagan2074 <151520985+jagan2074@users.noreply.github.com>
2024-11-06 14:25:50 +0530

    pfsd

---
+++ pg/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+
+    def __str__(self):
+        return self.title
+
